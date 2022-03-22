8264e5a865fda0713367d1810ad6c8c93368867a
Dylan-vong <dylan.vongsouvanh@students.mq.edu.au>
2022-03-22 21:09:46 +1100

    Update Client.java

--- Client.java
+++ Client.java
@@ -3,13 +3,15 @@ import java.io.*;
 
 class Client {
     public static void main (String args[]) {
-        Socket s = null;
+        Socket s = null; //Create new socket
         try{
             int port = 50000; //Sets the server port to 50000
             s = new Socket("localhost", port); //Initialises the socket
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            DataOutputStream out = new DataOutputStream( s.getOutputStream());
-            out.write(("HELO\n").getBytes()); //HELO
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream())); //Reads the data from input
+            DataOutputStream out = new DataOutputStream(s.getOutputStream()); //Writes output to server
+            
+            //Handshake Protocol
+            out.write(("HELO\n").getBytes()); //Client sends "HELO" to server
             String data = in.readLine();
             System.out.println("Received: "+ data) ; // Server receives "HELO"
             out.write(("AUTH Dylan\n").getBytes()); //Authentication
@@ -26,7 +28,8 @@ class Client {
             System.out.println("IO:"+e.getMessage());
         }
         if(s!=null) try {
-            s.close();}
+            s.close(); //Ends connection to server if no more inputs
+        }
             catch (IOException e){
                 System.out.println("close:"+e.getMessage());
             }
